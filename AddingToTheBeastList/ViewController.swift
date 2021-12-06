a6d6620c920a4e4f1fe04d4b92af6f845859dcd4
Mohammad Jaha <miro.tune@gmail.com>
2021-12-06 11:34:55 +0000

    Uploading Ass

--- AddingToTheBeastList/ViewController.swift
+++ AddingToTheBeastList/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var beastEntry: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+
+    var tasks = ["Exercise for 30 minutes", "Wireframe for some project", "Do laundry"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
 
-
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        guard let task = beastEntry.text else {return}
+        if task.isEmpty {return}
+        tasks.append(task)
+        tableView.reloadData()
+        beastEntry.text = ""
+    }
 }
 
+extension ViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        cell.textLabel?.textAlignment = .center
+        return cell
+    }
+}
